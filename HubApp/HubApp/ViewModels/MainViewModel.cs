8848dd66c24adaa989622e05402e1890adad5ec5
jorgecensi <jorgecensi@gmail.com>
2017-04-24 21:21:43 -0300

    Adicionado uma ViewModel e feito a ligação(Binding) desta viewmodel com a pagina principal alterando o code behind da MainPage.xaml

---
+++ HubApp/HubApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿
+namespace HubApp.ViewModels
+{
+    public class MainViewModel
+    {
+        public string Descricao { get; set; }
+
+        public MainViewModel()
+        {
+            Descricao = "Ola mundo";
+        }
+    }
+}
