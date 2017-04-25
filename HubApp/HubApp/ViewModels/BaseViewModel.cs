bd0fd1c2cdd1da718c7f3c96ba4a1e95b6ca1b1d
jorgecensi <jorgecensi@gmail.com>
2017-04-24 23:00:43 -0300

    Adicionado a BaseViewModel com métodos que são chamados na mudança de valor das propriedades. Deixando a MainViewModel mais limpa.

---
+++ HubApp/HubApp/ViewModels/BaseViewModel.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace HubApp.ViewModels
+{
+    public class BaseViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Este método irá invocar um evento de alteração de propriedade passando o nome da propriedade que chamou ela,
+        /// utilizando o CallerMemberName.
+        /// </summary>
+        /// <param name="propertyName">nome da propriedade que chamou o metodo</param>
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            /// O "PropertyChanged?" é utilizado no lugar de if(PropertyChanged != null)
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Altera o valor do parametro passado por referencia pelo value somente se for diferente.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="storage"></param>
+        /// <param name="value"></param>
+        /// <param name="propertyName"></param>
+        /// <returns></returns>
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                return false;
+            }
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
--- HubApp/HubApp/ViewModels/MainViewModel.cs
+++ HubApp/HubApp/ViewModels/MainViewModel.cs
@@ -1,13 +1,36 @@
-﻿
+﻿using System.Threading.Tasks;
+
 namespace HubApp.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel: BaseViewModel
     {
-        public string Descricao { get; set; }
+       
+
+        private string _descricao;
+
+        public string Descricao
+        {
+            get { return _descricao; }
+            set { SetProperty(ref _descricao, value); }
+        }
+
 
         public MainViewModel()
         {
             Descricao = "Ola mundo";
+            Task.Delay(3000).ContinueWith(async t =>
+            {
+                Descricao = "O texto Mudou";
+                for (int i = 1; i <= 10; i++)
+                {
+                    await Task.Delay(1000);
+                    Descricao = $"O texto Mudou {i}";
+
+                }
+
+            });
         }
+
+        
     }
 }
