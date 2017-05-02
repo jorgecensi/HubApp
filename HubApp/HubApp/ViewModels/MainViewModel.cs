ca1e60153bb0266cd1104c964201a550e536a240
jorgecensi <jorgecensi@gmail.com>
2017-05-02 20:02:41 -0300

    Adicionado ListView fazendo binding com ObservableCollection

--- HubApp/HubApp/ViewModels/MainViewModel.cs
+++ HubApp/HubApp/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Xamarin.Forms;
+﻿using System.Collections.ObjectModel;
+using Xamarin.Forms;
 
 namespace HubApp.ViewModels
 {
@@ -16,18 +17,22 @@ namespace HubApp.ViewModels
                     SearchCommand.ChangeCanExecute();
             }
         }
+        public ObservableCollection<string> Resultados { get; }
 
         public Command SearchCommand { get; }
 
         public MainViewModel()
         {
             SearchCommand = new Command(ExecuteSearchCommand,CanExecuteSearchCommand);
+
+            Resultados = new ObservableCollection<string>(new []{ "abc", "cde" });
         }
 
         async void ExecuteSearchCommand()
         {
             // não é recomendável deixar o código abaixo, é somente para estudos
             await App.Current.MainPage.DisplayAlert("HubApp", "mensagem", "OK");
+            Resultados.Add("ItenLIsta");
         }
 
         bool CanExecuteSearchCommand()
